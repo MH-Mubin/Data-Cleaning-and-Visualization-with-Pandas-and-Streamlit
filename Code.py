de50cf84da2900b305614cfd3641e715c394adf8
Mahmud Hasan Mubin <59206457+MH-Mubin@users.noreply.github.com>
2023-06-19 10:53:27 +0600

    Code.py

--- Code.py
+++ Code.py
@@ -2,10 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-
+#read the text file
 read_file = pd.read_csv (r"C:\Users\DELL\Desktop\Pro\dataset.txt", sep=' ')
+#convert to csv
 read_file.to_csv (r"C:\Users\DELL\Desktop\Pro\dataset.csv", index=None)
-
+#store the csv file
 data = pd.read_csv(r"C:\Users\DELL\Desktop\Pro\dataset.csv")
 
 count_col = data.shape[0]
@@ -23,6 +24,7 @@ data.to_csv(r"C:\Users\DELL\Desktop\Pro\dataset_new.csv", index=False)
 
 data.dtypes
 
+#histogram
 plt.hist([data['Deaths'], data['Confirmed']],  color=['blue', 'red'], label = ['Deaths', 'Confirmed'], density='True')
 plt.legend()
 plt.title('Death and Confirmed Histogram')
@@ -30,6 +32,7 @@ plt.title('Death and Confirmed Histogram')
 data.rename(columns={'Province_State': 'Province_State/object', 'Country_Region' : 'Country_Region/object', 'Lat' : 'Lat/float64', 'Long' : 'Long/float64', 'Date' : 'Date/object', 'Confirmed' : 'Confirmed/float64', 'Deaths' : 'Deaths/float64'}, inplace=True)
 data.to_csv(r"C:\Users\DELL\Desktop\Pro\dataset_new.csv", index=False)
 
+#map
 data_map = pd.read_csv(r"C:\Users\DELL\Desktop\Pro\dataset_new.csv")
 data_map.rename(columns={'Lat': 'lat', 'Long' : 'lon' }, inplace=True)
 st.map(data_map)
